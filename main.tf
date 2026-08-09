2e533bfa78cde195774d50a8ca87ab71bad1eb92
Kalamkarkishor <34445536+kalamkarkishor1993@users.noreply.github.com>
2026-08-09 09:32:27 +0000

    Migrate to S3 remote backend for shared state

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "kalamkarkishor-terraform-state"
+    key    = "terraform-aws-project/terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
 
 provider "aws" {
